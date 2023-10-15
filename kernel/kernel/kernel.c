d03510aedb9a8d29262de8fa544101da3bb57d48
Blizzardfur <111100393+Blizzardfur-Maxxx@users.noreply.github.com>
2023-10-15 00:39:21 -0500

    a

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,7 +2,13 @@
 
 #include <kernel/tty.h>
 
+int i = 1;
+
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, Welcome too BlizzardOS!\n");
+	printf("Hello, Welcome to BlizzardOS!\n");
+	//terminal_setcolor(VGA_COLOR_BLUE);
+	for (int i = 0; i < 100; i++) {
+    	printf("big balls:3\n");
+	}		
 }
